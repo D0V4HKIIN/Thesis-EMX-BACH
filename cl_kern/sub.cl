c2329f05dce9d83471596741580bcf25e85a6dee
HWMindroad <hugo.wilhelmsson@mindroad.se>
2024-02-16 16:33:00 +0100

    Somewhat fixed kernel sum multiplication. Still poor precision.

--- cl_kern/sub.cl
+++ cl_kern/sub.cl
@@ -1,13 +1,14 @@
 void kernel sub(global const double *S, global const double *I,
                 global double *D,
-                const long convWidth, const long w, const long h) {
+                const long convWidth, const long w, const long h,
+                const double convFactor, const double finalFactor) {
   const int id = get_global_id(0);
   const long x = id % w;
   const long y = id / w;
 
   if(x >= convWidth / 2 && x < w - convWidth / 2 && y >= convWidth / 2 &&
      y < h - convWidth / 2) {
-    D[id] = (I[id] - S[id]);
+    D[id] = (I[id] - S[id] * convFactor) * finalFactor;
   } else {
     D[id] = 1e-30;
   }
--- include/bach.h
+++ include/bach.h
@@ -19,10 +19,10 @@ struct ClData
 void init(Image &templateImg, Image &scienceImg, ImageMask &mask, ClData& clData);
 void sss(const Image &templateImg, const Image &scienceImg, ImageMask &mask, std::vector<Stamp> &templateStamps, std::vector<Stamp> &sciStamps);
 void cmv(const Image &templateImg, const Image &scienceImg, ImageMask &mask, std::vector<Stamp> &templateStamps, std::vector<Stamp> &sciStamps, const Kernel &convolutionKernel);
-void cd(Image &templateImg, Image &scienceImg, ImageMask &mask, std::vector<Stamp> &templateStamps, std::vector<Stamp> &sciStamps);
+bool cd(Image &templateImg, Image &scienceImg, ImageMask &mask, std::vector<Stamp> &templateStamps, std::vector<Stamp> &sciStamps);
 void ksc(const Image &templateImg, const Image &scienceImg, ImageMask &mask, std::vector<Stamp> &templateStamps, Kernel &convolutionKernel);
-void conv(const Image &templateImg, const Image &scienceImg, ImageMask &mask, Image &convImg, Kernel &convolutionKernel,
+double conv(const Image &templateImg, const Image &scienceImg, ImageMask &mask, Image &convImg, Kernel &convolutionKernel, bool convTemplate,
           const cl::Context &context, const cl::Program &program, cl::CommandQueue &queue);
-void sub(const Image &convImg, const Image &scienceImg, const ImageMask &mask, Image &diffImg,
+void sub(const Image &convImg, const Image &scienceImg, const ImageMask &mask, Image &diffImg, bool convTemplate, double invKernSum,
          const cl::Context &context, const cl::Program &program, cl::CommandQueue &queue);
 void fin(const Image &convImg, const Image &diffImg);
