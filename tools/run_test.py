9a1969028bd256fdbfd37a5801b7e914d457b03f
HWMindroad <hugo.wilhelmsson@mindroad.se>
2024-02-16 13:23:13 +0100

    Refactored run_test.py

--- tools/run_test.py
+++ tools/run_test.py
@@ -79,9 +79,48 @@ def diff_fits(h_path, b_path):
 
     return max_error_abs, max_error_rel, wrong_nans
 
-def main(args):
+def run_test(test_index):
+    (id, science_name, template_name, conv_name, sub_name, max_abs_error, max_rel_error) = TEST_TABLE[test_index]
+
+    print(f"{INFO_COLOR}Running test {id}...")
+
     exe_path = BUILD_PATH / "BACH.exe"
 
+    exe_args = [str(exe_path)]
+    exe_args += ["-ip", str(RES_PATH)]
+    exe_args += ["-s", f"{science_name}.fits"]
+    exe_args += ["-t", f"{template_name}.fits"]
+    exe_args += ["-op", str(OUTPUT_PATH / f"test{id}_")]
+
+    start_time = time.time()
+
+    with open(OUTPUT_PATH / f"test{id}_out.txt", "w") as out_stream:
+        if not subprocess.run(args=exe_args, stdout=out_stream, stderr=out_stream):
+            print(f"{ERROR_COLOR}X-BACH exited with an error code!")
+            return False
+
+    CONV_OUT_PATH = OUTPUT_PATH / f"test{id}_diff.fits"
+    SUB_OUT_PATH = OUTPUT_PATH / f"test{id}_sub.fits"
+
+    if not CONV_OUT_PATH.exists() or not SUB_OUT_PATH.exists():
+        print("At least one X-BACH output is missing. The program did not run correctly.")
+        return False
+
+    end_time = time.time()
+    test_time = end_time - start_time
+
+    conv_abs_err, conv_rel_err, conv_wrong_nans = diff_fits(TEST_PATH / f"{conv_name}.fits", CONV_OUT_PATH)
+    sub_abs_err, sub_rel_err, sub_wrong_nans = diff_fits(TEST_PATH / f"{sub_name}.fits", SUB_OUT_PATH)
+
+    print(f"Test took {test_time:.2f} seconds")
+    print(f"Convolution errors: {conv_abs_err:.2e} (abs), {conv_rel_err:.2e} (rel) and {conv_wrong_nans} (NaN)")
+    print(f"Subtraction errors: {sub_abs_err:.2e} (abs), {sub_rel_err:.2e} (rel) and {sub_wrong_nans} (NaN)")
+
+    return conv_abs_err > max_abs_error[0] or conv_rel_err > max_rel_error[0] or\
+        sub_abs_err > max_abs_error[1] or sub_rel_err > max_rel_error[1] or\
+        conv_wrong_nans > 0 or sub_wrong_nans > 0
+
+def main(args):
     print(f"There are a total of {len(TEST_TABLE)} tests to run")
     print(f"{INFO_COLOR}NOTE: running X-BACH from \"{BUILD_PATH.resolve()}\"")
 
@@ -103,60 +142,23 @@ def main(args):
             shutil.rmtree(os.path.join(root, d))
 
     failed_tests = 0
+    total_tests = 0
 
-    for test_case in TEST_TABLE:
-        (id, science_name, template_name, conv_name, sub_name, max_abs_error, max_rel_error) = test_case
-
-        print(f"{INFO_COLOR}Running test {id}...")
-
-        exe_args = [str(exe_path)]
-        exe_args += ["-ip", str(RES_PATH)]
-        exe_args += ["-s", f"{science_name}.fits"]
-        exe_args += ["-t", f"{template_name}.fits"]
-        exe_args += ["-op", str(OUTPUT_PATH / f"test{id}_")]
-
-        start_time = time.time()
-
-        with open(OUTPUT_PATH / f"test{id}_out.txt", "w") as out_stream:
-            if not subprocess.run(args=exe_args, stdout=out_stream, stderr=out_stream):
-                failed_tests += 1
-                print(f"{ERROR_COLOR}X-BACH exited with an error code!")
-                print()
-                continue
-
-        CONV_OUT_PATH = OUTPUT_PATH / f"test{id}_diff.fits"
-        SUB_OUT_PATH = OUTPUT_PATH / f"test{id}_sub.fits"
-
-        if not CONV_OUT_PATH.exists() or not SUB_OUT_PATH.exists():
-            failed_tests += 1
-            print("At least one X-BACH output is missing. The program did not run correctly.")
-            print()
-            continue
-
-        end_time = time.time()
-        test_time = end_time - start_time
-
-        conv_abs_err, conv_rel_err, conv_wrong_nans = diff_fits(TEST_PATH / f"{conv_name}.fits", CONV_OUT_PATH)
-        sub_abs_err, sub_rel_err, sub_wrong_nans = diff_fits(TEST_PATH / f"{sub_name}.fits", SUB_OUT_PATH)
-
-        print(f"Test took {test_time:.2f} seconds")
-        print(f"Convolution errors: {conv_abs_err:.2e} (abs), {conv_rel_err:.2e} (rel) and {conv_wrong_nans} (NaN)")
-        print(f"Subtraction errors: {sub_abs_err:.2e} (abs), {sub_rel_err:.2e} (rel) and {sub_wrong_nans} (NaN)")
-
-        test_fail = conv_abs_err > max_abs_error[0] or conv_rel_err > max_rel_error[0] or\
-            sub_abs_err > max_abs_error[1] or sub_rel_err > max_rel_error[1] or\
-            conv_wrong_nans > 0 or sub_wrong_nans > 0
+    for i in range(len(TEST_TABLE)):
+        test_id = TEST_TABLE[i][0]
+        test_fail = run_test(i)
         
         if test_fail:
             failed_tests += 1
-            print(f"{ERROR_COLOR}Test {id} failed on science image {science_name} and template image {template_name}!")
+            print(f"{ERROR_COLOR}Test {test_id} failed!")
         else:
-            print(f"{PASS_COLOR}Test {id} succeeded!")
+            print(f"{PASS_COLOR}Test {test_id} succeeded!")
 
+        total_tests += 1
         print()
 
     if failed_tests > 0:
-        print(f"{ERROR_COLOR}{failed_tests} tests failed!")
+        print(f"{ERROR_COLOR}{failed_tests} / {total_tests} tests failed!")
 
         sys.exit(1)
     else:
