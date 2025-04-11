2ca747333cb6a0545a24e4915d7826bbf943b8b5
Jonas Bonaudet <jonas.bonnaudet@gmail.com>
2025-04-11 13:54:46 +0200

    forgor

--- clone_all_https.sh
+++ clone_all_https.sh
@@ -7,7 +7,7 @@ cd BACH
 git checkout bach-ref
 cd ..
 
-git clone git@github.com:D0V4HKIIN/Thesis-EMX-BACH.git XBACH
+git clone https://github.com/D0V4HKIIN/Thesis-EMX-BACH.git XBACH
 cd XBACH
 git checkout x-bach-ref
 cd ..
