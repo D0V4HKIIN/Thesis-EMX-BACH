b63497a93372a18d82e6caec50f81dd5b32170b9
Jonas Bonaudet <jonas.bonnaudet@gmail.com>
2025-04-11 13:39:09 +0200

    script to clone all needed repos for benchmarking

---
+++ clone_all.sh
@@ -0,0 +1,13 @@
+cd ..
+
+git clone git@github.com:D0V4HKIIN/hotpants.git
+
+git clone git@github.com:D0V4HKIIN/Thesis-EMX-BACH.git BACH
+cd BACH
+git checkout bach-ref
+cd ..
+
+git clone git@github.com:D0V4HKIIN/Thesis-EMX-BACH.git XBACH
+cd XBACH
+git checkout x-bach-ref
+cd ..
